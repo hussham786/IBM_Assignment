f4e010dd349d5e0366ffe642eb5aced15842909d
hussham786 <35285868+hussham786@users.noreply.github.com>
2019-05-10 22:08:07 +0530

    Add files via upload

---
+++ assignment_9_may_2019/SecondMostFrequentChar.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class SecondMostFrequentChar{
+	static final int NO_OF_CHARS = 256; 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String str = sc.nextLine();
+
+		System.out.println("The second most frequent character is: " + new SecondMostFrequentChar().secMostFreq(str));
+	}
+
+	char secMostFreq(String str){
+		int[] count = new int[NO_OF_CHARS]; 
+		int first = 0, second = 0; 
+        for (int i = 0; i < str.length(); i++) 
+            (count[str.charAt(i)])++;
+
+        for (int i = 0; i < NO_OF_CHARS; i++) 
+        { 
+            
+            if (count[i] > count[first]) 
+            { 
+                second = first; 
+                first = i; 
+            } 
+       
+        
+            else if (count[i] > count[second] && 
+                     count[i] != count[first]) 
+                second = i; 
+        } 
+       
+        return (char)second; 
+    }  
+	}
